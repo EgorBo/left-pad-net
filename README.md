061f5553b94c69a2256eb2d7c9ea449a6ef16e37
EgorBo <egorbo@gmail.com>
2016-03-24 13:58:13 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # left-pad-net
-left-pad-net
+Don't be so NIH, use https://www.nuget.org/packages/left-pad/ instead of reinventing the Wheel.
---
+++ left-pad/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace left_pad
+{
+    public static class StringExtensions
+    {
+        public static string LeftPad(char ch, int len)
+        {
+            var str = "";
+            var i = -1;
+            len = len - str.Length;
+            while (++i < len)
+            {
+                str = ch + str;
+            }
+            return str;
+        }
+    }
+}
