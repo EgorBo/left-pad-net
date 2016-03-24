a9e77ba41801eda317a151aa78d861f784ec0c36
EgorBo <egorbo@gmail.com>
2016-03-24 14:18:43 +0100

    Add tests, fix leftpad

---
+++ left-pad-tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using left_pad;
+
+namespace left_pad_tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void LeftPadWorks()
+        {
+            Assert.Equal("0001", "1".LeftPad('0', 4));
+            Assert.Equal("0001", "0001".LeftPad('1', 4));
+            Assert.Equal("0000", "".LeftPad('0', 4));
+            Assert.Equal("123", "123".LeftPad('0', 2));
+            Assert.Throws<InvalidOperationException>(() => "123".LeftPad('0', -1));
+        }
+    }
+}
--- left-pad/StringExtensions.cs
+++ left-pad/StringExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace left_pad
+﻿using System;
+
+namespace left_pad
 {
     public static class StringExtensions
     {
-        public static string LeftPad(char ch, int len)
+        public static string LeftPad(this string str, char ch, int len)
         {
-            var str = "";
+            if (len <= 0)
+                throw new InvalidOperationException();
+
+            if (string.IsNullOrEmpty(str))
+                return new string(ch, len);
+
             var i = -1;
             len = len - str.Length;
             while (++i < len)
